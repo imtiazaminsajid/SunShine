b86c40fc4ffe05840f8f6650b98bf4332e3b0649
imtiazaminsajid <ambition.sajid@gmail.com>
2019-01-24 00:04:07 +0600

    Models Class are done

---
+++ app/src/main/java/com/example/ambit/sunshine/Common/Common.java
@@ -0,0 +1,6 @@
+package com.example.ambit.sunshine.Common;
+
+public class Common {
+
+    public static final String API_ID = "eabe8de4211468c77e9dcda6150b0cda";
+}
---
+++ app/src/main/java/com/example/ambit/sunshine/Retrofit/IOpenWeatherMap.java
@@ -0,0 +1,18 @@
+package com.example.ambit.sunshine.Retrofit;
+
+import com.example.ambit.sunshine.Model.WeatherResult;
+import com.google.android.gms.common.data.DataBufferObserver;
+
+import java.util.Observer;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IOpenWeatherMap {
+    @GET("weather")
+    Observable<WeatherResult> getWeatherByLatLng(@Query("lat") String lat,
+                                                 @Query("lon") String lng,
+                                                 @Query("appid") String appid,
+                                                 @Query("units") String unit);
+}
